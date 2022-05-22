4aee4976ae75feaeb96e98ab1d9e56dd5df821b9
GreedNeSS <ruslan199126@gmail.com>
2022-05-22 17:41:10 +0300

    Added simple example.

--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Use(async (context, next) =>
+{
+    await next();
+
+    if (context.Response.StatusCode == 404)
+    {
+        await Results.NoContent().ExecuteAsync(context);
+    }
+});
+
+app.Map("/", MainPage);
+app.Map("/users", () => Results.Content("Users Page"));
 
 app.Run();
+
+IResult MainPage()
+{
+    return Results.Text("Main Page");
+}
